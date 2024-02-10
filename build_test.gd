15fdbc078c2e97916fd13692ea36317b6de06a31
standstilldigitalmedia <93889983+standstilldigitalmedia@users.noreply.github.com>
2024-02-10 01:42:35 -0500

    Avatar button
    The Meshes button in the left side of the AWOC editor has been renamed to Avatar.
    
    An images button has been added to the left side of the of the AWOC editor. The images pane is blank for now but it will be the next pane to be completed.

--- build_test.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-@export var main_cam: Camera3D
-@export var target: Node3D
-@export var awoc: AWOCRes
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	awoc.avatar_res.deserialize_avatar(["Feet", "Hands", "Legs", "Torso"])
-	target.add_child(awoc.avatar_res.avatar)
